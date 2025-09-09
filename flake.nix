2615465ab83146b884768e1fccfa884a866aa074
mana-byte <manaikilaut@gmail.com>
2025-09-09 20:27:07 +0200

    modified(flake.nix)

--- flake.nix
+++ flake.nix
@@ -16,6 +16,9 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
+      config = {
+        allowUnfree = true;
+      };
     };
   in {
     nixosConfigurations = {
