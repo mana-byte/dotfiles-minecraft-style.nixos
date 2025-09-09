bda26a58e6c089c34f6d562a7a213247ca5fddf2
mana-byte <manaikilaut@gmail.com>
2025-09-09 20:29:25 +0200

    modified(flake.nix)

--- flake.nix
+++ flake.nix
@@ -3,26 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     home-manager.url = "github:nix-community/home-manager";
   };
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
     home-manager,
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config = {
-        allowUnfree = true;
-      };
-    };
-  in {
+  }: {
     nixosConfigurations = {
-      my-machine = pkgs.nixosSystem {
+      my-machine = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ({
